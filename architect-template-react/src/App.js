50c8257474641e34d44ac81710b8b15c89e12d7c
EmreZiyaEfe <emreziyae@gmail.com>
2022-09-18 02:08:42 +0300

    Website template sample using React

---
+++ architect-template-react/src/App.js
@@ -0,0 +1,19 @@
+import Footer from "./react-template-work/Footer";
+import Header from "./react-template-work/Header";
+import Navbar from "./react-template-work/Navbar";
+import PageContent from "./react-template-work/PageContent";
+import Image from "./react-template-work/Image";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <PageContent />
+      <Image />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ architect-template-react/src/react-template-work/PageContent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Projects from './Projects'
+import About from './About'
+import Contact from './Contact'
+
+
+function PageContent() {
+    return (
+        <>
+            {/* Page content */}
+            <div className="w3-content w3-padding" style={{ maxWidth: 1564 }}>
+                <Projects />
+                <About />
+                <Contact />
+            </div>
+        </>
+
+    )
+}
+
+export default PageContent
